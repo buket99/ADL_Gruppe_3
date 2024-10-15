6a79ab0f61529bb2194f3e79c89d47d2b32c4d7a
buket99 <kurtulus.buket.23@icloud.com>
2024-10-15 12:35:35 +0200

    added webcam module

---
+++ webcam.py
@@ -0,0 +1,20 @@
+import cv2
+
+input("Press the Enter key to continue: ")
+video = cv2.VideoCapture(0)
+a = 0
+
+while True:
+    a = a + 1
+    # Create a frame object
+    check, frame = video.read()
+    # show the frame while capturing the image
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):  # Press q to exit
+        break
+
+# save image
+showPic = cv2.imwrite("filename.jpg", frame)
+#print(showPic)
+video.release()
